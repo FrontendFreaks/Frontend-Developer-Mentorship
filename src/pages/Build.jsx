a5a59147c5dfe9201298ba9867487db5167eb785
Vishal Rajput <rajputvishal33786@gmail.com>
2023-06-02 16:07:14 +0530

    optimise Batch Introduction

--- src/pages/Build.jsx
+++ src/pages/Build.jsx
@@ -1,10 +1,21 @@
 import React from "react";
-import Introduction from "../components/BuildBatch/Introduction";
+import BatchIntroduction from "../components/BatchIntroduction";
 import StartLearning from "../components/BuildBatch/StartLearning";
+
 const Build = () => {
+  const batchHighlights = [
+    "The second batch focuses on ReactJS, Redux, Tailwind, and NextJS, enabling participants to create practical projects and enhance their frontend expertise.",
+    "For those with HTML, CSS, and JavaScript basics. Elevate skills and create with guidance.",
+    "Level up in frontend development: ReactJS, Redux, Tailwind, NextJS projects in the second batch.",
+  ];
+
   return (
     <div className="mt-32 py-6 px-10 ">
-      <Introduction />
+      <BatchIntroduction
+        batchName="Batch"
+        batchIntroVideo="https://youtu.be/F6WT-EwTBt4"
+        batchHighlights={batchHighlights}
+      />
       <StartLearning />
     </div>
   );
--- src/pages/Learn.jsx
+++ src/pages/Learn.jsx
@@ -1,10 +1,20 @@
-import React from "react";
-import Introduction from "./../components/LearnBatch/Introduction";
+import BatchIntroduction from "../components/BatchIntroduction";
 import StartLearning from "../components/LearnBatch/StartLearning";
+
 const Learn = () => {
+  const batchHighlights = [
+    "Beginner-friendly web development curriculum: Covers HTML, CSS, JavaScript, Git, and GitHub from scratch.",
+    "Comprehensive learning materials: Includes resources, assignments, cheat sheets, and over 100 questions for practice and testing.",
+    "Frontend Developer Essential Course: Equips students with essential skills and knowledge to excel in frontend development.",
+  ];
+
   return (
     <div className="mt-32 py-6 px-2 md:px-10 ">
-      <Introduction />
+      <BatchIntroduction
+        batchName="Learn"
+        batchIntroVideo="https://youtu.be/tI7R7YG4jEY"
+        batchHighlights={batchHighlights}
+      />
       <StartLearning />
     </div>
   );
