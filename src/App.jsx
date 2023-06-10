38b89bf71835f947983f2b1d3556440cff260ecb
Vishal Rajput <rajputvishal33786@gmail.com>
2023-06-10 15:12:37 +0530

    adding google and vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Analytics from "@vercel/analytics/react"
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import LearnBatch from "./pages/LearnBatch";
@@ -44,6 +45,7 @@ function App() {
           <Route path="learn/js/notes" element={<JSNotes />} />
         </Route>
       </Routes>
+      <Analytics />
     </BrowserRouter>
   );
 }
