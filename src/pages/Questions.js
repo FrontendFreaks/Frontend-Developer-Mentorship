ef671d519b09c6d4f4b8e01760880ddf1a12b499
sujitmemane <officialsujitmemane@gmail.com>
2023-06-16 22:30:01 +0530

    Added MCQ Feature in HTML

---
+++ src/pages/Questions.js
@@ -0,0 +1,145 @@
+export const htmlMCQQuestions = [
+  {
+    id: 1,
+    question: "What does HTML stand for?",
+    options: [
+      "Hyper Text Markup Language",
+      "Home Tool Markup Language",
+      "Hyperlinks and Text Markup Language",
+    ],
+    answer: "Hyper Text Markup Language",
+  },
+  {
+    id: 2,
+    question: "What is the correct HTML element for the largest heading?",
+    options: ["<h1>", "<heading>", "<head>", "<h6>"],
+    answer: "<h1>",
+  },
+  {
+    id: 3,
+    question:
+      "Which attribute is used to provide an additional advisory text about an element?",
+    options: ["title", "alt", "src", "href"],
+    answer: "title",
+  },
+  {
+    id: 4,
+    question: "Which HTML element is used to define an unordered list?",
+    options: ["<ul>", "<ol>", "<li>", "<list>"],
+    answer: "<ul>",
+  },
+  {
+    id: 5,
+    question:
+      "Which HTML attribute specifies an alternate text for an image, if the image cannot be displayed?",
+    options: ["alt", "title", "src", "href"],
+    answer: "alt",
+  },
+  {
+    id: 6,
+    question: "What is the correct HTML element for inserting a line break?",
+    options: ["<break>", "<hr>", "<br>", "<lb>"],
+    answer: "<br>",
+  },
+  {
+    id: 7,
+    question: "Which HTML element defines the title of a document?",
+    options: ["<head>", "<meta>", "<title>", "<header>"],
+    answer: "<title>",
+  },
+  {
+    id: 8,
+    question: "Which HTML element is used to define a table row?",
+    options: ["<td>", "<th>", "<tr>", "<table>"],
+    answer: "<tr>",
+  },
+  {
+    id: 9,
+    question:
+      "In HTML, which attribute is used to specify that an input field must be filled out?",
+    options: ["placeholder", "required", "validate", "mandatory"],
+    answer: "required",
+  },
+  {
+    id: 10,
+    question:
+      "Which HTML element is used to define the title of a section within an article?",
+    options: ["<section>", "<article>", "<title>", "<heading>"],
+    answer: "<heading>",
+  },
+  {
+    id: 11,
+    question: "What is the correct HTML element for playing video files?",
+    options: ["<media>", "<video>", "<movie>", "<play>"],
+    answer: "<video>",
+  },
+  {
+    id: 12,
+    question:
+      "Which HTML attribute specifies the URL of an external script file?",
+    options: ["<src>", "<href>", "<link>", "<script>"],
+    answer: "<src>",
+  },
+  {
+    id: 13,
+    question: "What is the correct HTML element for defining emphasized text?",
+    options: ["<italic>", "<strong>", "<em>", "<emphasis>"],
+    answer: "<em>",
+  },
+  {
+    id: 14,
+    question:
+      "Which HTML element is used to define the footer of a document or a section?",
+    options: ["<footer>", "<bottom>", "<end>", "<section>"],
+    answer: "<footer>",
+  },
+  {
+    id: 15,
+    question:
+      "Which HTML element is used to define the navigation links in a document?",
+    options: ["<nav>", "<link>", "<navbar>", "<navigate>"],
+    answer: "<nav>",
+  },
+  {
+    id: 16,
+    question:
+      "In HTML, which element is used to specify a header for a document or section?",
+    options: ["<head>", "<header>", "<title>", "<h1>"],
+    answer: "<header>",
+  },
+  {
+    id: 17,
+    question: "Which HTML element is used to define the caption of a table?",
+    options: ["<caption>", "<summary>", "<table>", "<title>"],
+    answer: "<caption>",
+  },
+  {
+    id: 18,
+    question: "What is the correct HTML for creating a hyperlink?",
+    options: [
+      '<a url="http://example.com">Example</a>',
+      '<a href="http://example.com">Example</a>',
+      "<a>http://example.com</a>",
+      '<link src="http://example.com">Example</link>',
+    ],
+    answer: '<a href="http://example.com">Example</a>',
+  },
+  {
+    id: 19,
+    question:
+      "Which HTML element is used to define the main content of a document?",
+    options: ["<content>", "<main>", "<body>", "<container>"],
+    answer: "<main>",
+  },
+  {
+    id: 20,
+    question: "What is the correct HTML for inserting an image?",
+    options: [
+      '<img href="image.jpg" alt="MyImage">',
+      '<image src="image.jpg" alt="MyImage">',
+      '<img src="image.jpg" alt="MyImage">',
+      '<picture src="image.jpg" alt="MyImage">',
+    ],
+    answer: '<img src="image.jpg" alt="MyImage">',
+  },
+];
