7a7f25d86032d545a288c042c27a39f937c1a6ad
Vishal Rajput <rajputvishal33786@gmail.com>
2023-06-18 15:49:15 +0530

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -10,25 +10,29 @@ import LearnReact from "./pages/LearnReact";
 import LearnInterviewQs from "./pages/LearnInterviewQs";
 import Hire from "./pages/Hire";
 import Build from "./pages/Build";
+import { Analytics } from "@vercel/analytics/react";
 import "./App.css";
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="learn" element={<LearnBatch />} />
-          <Route path="hire" element={<Hire />} />
-          <Route path="build" element={<Build />} />
-          <Route path="learn/html" element={<LearnHtml />} />
-          <Route path="learn/css" element={<LearnCss />} />
-          <Route path="learn/git" element={<LearnGit />} />
-          <Route path="learn/js" element={<LearnJs />} />
-          <Route path="build/react" element={<LearnReact />} />{" "}
-          <Route path="build/interviewqs" element={<LearnInterviewQs />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="learn" element={<LearnBatch />} />
+            <Route path="hire" element={<Hire />} />
+            <Route path="build" element={<Build />} />
+            <Route path="learn/html" element={<LearnHtml />} />
+            <Route path="learn/css" element={<LearnCss />} />
+            <Route path="learn/git" element={<LearnGit />} />
+            <Route path="learn/js" element={<LearnJs />} />
+            <Route path="build/react" element={<LearnReact />} />{" "}
+            <Route path="build/interviewqs" element={<LearnInterviewQs />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      <Analytics />
+    </>
   );
 }
 
