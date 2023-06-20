a0fab916298b300b4de40176e59163a71759474f
Prashant Mishra <prashantmishra111@outlook.com>
2023-06-20 15:56:45 +0530

    Align "About the Founder" heading fix!!

--- src/components/Home/AboutFounder.jsx
+++ src/components/Home/AboutFounder.jsx
@@ -7,11 +7,15 @@ const AboutFounder = () => {
       <div className="mt-24 py-6 px-5 md:px-20 flex flex-col space-y-12 items-center justify-center">
         <div className="flex flex-col items-center   md:flex-row md:items-start justify-between space-x-2 space-y-8 md:space-y-0">
           <div className="md:1/2 flex flex-col items-between justify-between space-y-3 text-white    ">
-            <img src={FounderImage} className="max-w-[550px] h-84 rounded  " />
+            <img
+              src={FounderImage}
+              className="w-[550px] h-84 rounded"
+              alt="founder image"
+            />
             <h1 className="text-4xl font-bold text-center ">Vishal Rajput</h1>
           </div>
           <div className="md:w-1/2 flex text-[1rem]  -mt-8 flex-col items-start justify-between space-y-5 text-white  p-1 ">
-            <div className="text-left font-bold text-5xl text-white flex items-center md:items-start justify-center flex-col space-y-3 ">
+            <div className="text-left font-bold text-5xl text-white flex items-start md:items-start justify-center flex-col space-y-3 ">
               <h1>About The Founder</h1>
               <div className="w-28 h-2 bg-[#687eff] rounded"></div>
             </div>
