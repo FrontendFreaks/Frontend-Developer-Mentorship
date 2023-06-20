e28adfb5c2b00e0327928f48ed60b46c9edf9f1e
Sujit <mnamesujit@gmail.com>
2023-06-20 20:36:10 +0530

    fixed: y-axis-space between website slogan and home page buttons (Join Community/ Join Mentorship button)

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,9 +1,9 @@
 import HeroImg from "./../../assets/hero.jpeg";
 const Hero = () => {
   return (
-    <div className="mt-4 py-24 px-5 md:px-10  flex flex-col-reverse md:flex-row items-center md:items-center justify-between bg-[#131313] ">
-      <div className="md:w-1/2 md:-mt-28  flex flex-col items-center md:items-start justify-between space-y-4  p-2 space-y-10">
-        <h1 className="text-5xl md:text-7xl text-center md:text-left font-bold capitalize max-w-xl mt-9 leading-18 text-white">
+    <div className="mt-4 py-24 px-5 md:px-10  flex flex-col-reverse md:flex-row items-center md:items-center justify-between bg-[#131313]">
+      <div className="md:w-1/2 md:-mt-28  flex flex-col items-center md:items-start justify-between space-y-4 p-2">
+        <h1 className="text-5xl md:text-7xl text-center md:text-left font-bold capitalize max-w-xl mt-9 leading-18 text-white mb-8">
           Unleash your{" "}
           <span className="text-[#687eff] text-6xl md:text-7xl">
             {" "}
