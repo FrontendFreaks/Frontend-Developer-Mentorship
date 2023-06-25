8fd749f2b308536262e10f6556a7eb61d0c53d5b
themodernmonk7 <sauravavishek@gmail.com>
2023-06-25 18:31:27 +0530

    feat: google analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import ReactGA from "react-ga4"
+
+const measurementID = "G-VWVCP2N7SS"
+ReactGA.initialize(measurementID)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
